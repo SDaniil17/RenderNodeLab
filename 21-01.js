66e30eb20f46e45c04304ff92c67c6748e1726c2
“Sokolovskiy <sokolovskiy.daniil.1996@mail.ru>
2023-04-03 10:20:07 +0300

    last

---
+++ 21-01.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser')
+const app = express();
+
+const hbs = require("express-handlebars").create({
+    extname: ".hbs",
+    defaultLayout: "index",
+    layoutsDir: "views",
+})
+
+const PORT = 3000
+app.engine(".hbs", hbs.engine);
+//установливаем Handlebars в качестве движка представлений в Express
+app.set("view engine", ".hbs");
+app.use('/static', express.static(path.join(__dirname, '/static')));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
+
+const appRoutes = require('./routes/appRoutes');
+
+appRoutes(app)
+
+const server = app.listen(PORT)
+console.log('server running on 3000 port')
